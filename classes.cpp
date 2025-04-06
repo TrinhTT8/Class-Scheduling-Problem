7b2da5783340df5186ffe3415f5e8d5dfc891fb7
EmmittFive <sorufive@gmail.com>
2025-04-06 14:33:06 -0500

    Update classes.cpp
    fixed numbers

--- classes.cpp
+++ classes.cpp
@@ -23,11 +23,11 @@ void printClassDetails(const unordered_map<string, ClassInfo>& classes) {
 		cout << "  Days: ";
 	    	for (int day: days) {
             		switch (day) {
-                		case 2: cout << "Monday "; break;
-                		case 3: cout << "Tuesday "; break;
-                		case 4: cout << "Wednesday "; break;
-                		case 5: cout << "Thursday "; break;
-                		case 6: cout << "Friday "; break;
+                		case 1: cout << "Monday "; break;
+                		case 2: cout << "Tuesday "; break;
+                		case 3: cout << "Wednesday "; break;
+                		case 4: cout << "Thursday "; break;
+                		case 5: cout << "Friday "; break;
 				default: break;
 			}
 		}
